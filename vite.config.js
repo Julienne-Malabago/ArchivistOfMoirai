0a96bb835d82abf0e771c2bde5cb5a60ca094445
Julienne <julienne.h.malabago@gmail.com>
2025-12-04 16:22:08 +0800

    Confirmed successful local build and final path fixes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // ADD THIS LINE TO FIX NETLIFY PATH ISSUES
+  // *** ADD THIS LINE ***
   base: './', 
   
   plugins: [
